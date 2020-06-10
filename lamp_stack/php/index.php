30a38d41a58be1c5df509f43cdb5a7c53c728daf
pro-gramming <dineshneerajb03@gmail.com>
2020-06-10 13:24:31 +0000

    connected to mysql

--- lamp_stack/php/index.php
+++ lamp_stack/php/index.php
@@ -1,5 +1,14 @@
 <?php 
 
- echo "Hello-world";
+$host = 'db';
+$user = 'dinesh';
+$password = 1234;
+$db = 'test_db';
 
+$connection = new mysqli($host, $user, $password, $db);
+if($connection->connect_error){
+   echo "connection failed" . $connection->connect_error;
+   }
+echo "successfully connected to mysql <br />";
+echo "does it works";
 ?>
